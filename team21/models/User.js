b40b7bdcbabfc1b2097dffe1cbd75428a73bb87b
Will Owuama <willowuama@gmail.com>
2019-10-18 21:37:56 -0500

    Built out User class

--- team21/models/User.js
+++ team21/models/User.js
@@ -1,7 +1,23 @@
 /**
  * Model for the App user
+ * UserID
  * Name
  * Email
  * Phone number
  * Medical Issue
  */
+
+ export default class User {
+    userID: int;
+    name: string;
+    email: string;
+    phoneNumber: string;
+
+    constructor(){
+       userID = 12345;
+       name = "Carl Banks";
+       email = "email@email.com";
+       phoneNumber = "832-212-9071";
+       medicalIssue = "Epilespy";
+    }
+ }
